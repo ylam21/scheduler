f21ce77aee52846ebdf59d0b3298d98aae3fa56b
Ondrej <ondrej@dell-latitude.home>
2025-03-17 20:54:05 +0100

    logic for recursion done

--- main.c
+++ main.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
 
-#define ROW 4
-#define COL 2
+#define ROWS 4
+#define COLS 2
+#define TOTAL 10
 
-int is_dupl(int matrix[ROW][COL], int r_pos, int c_pos, int n) {
+int is_dupl(int matrix[ROWS][COLS], int row, int col, int n) {
 	int i = 0;
-	while (i < c_pos) {
-		if (n == matrix[r_pos][i])
+	while (i < col) {
+		if (n == matrix[row][i])
 			return 1;
 		i++;
 	}
@@ -15,36 +16,36 @@ int is_dupl(int matrix[ROW][COL], int r_pos, int c_pos, int n) {
 }
 
 
-int is_ok(int matrix[ROW][COL],int r_pos, int c_pos, int n) {
+int is_ok(int matrix[ROWS][COLS],int row, int col, int num) {
 	//check appearance
 	//check repetition, duplicates DONE
 	//check if avail
+	return 1;
 }
 
-int solve(int matrix[ROW][COL],int r_pos, int c_pos, int total) {
-	if (r_pos == ROW + 1 && c_pos == COL + 1) {
+int solve(int matrix[ROWS][COLS],int row, int col, int total) {
+	if (row == ROWS)
 		return 1;
-	}
 	int i = 0;
 	while (i < total) {
-		matrix[r_pos][c_pos] = i;
-		if (is_ok(matrix, r_pos, c_pos, i)) {
-			solve(matrix,r_pos++,c_pos++,total);
+		matrix[row][col] = i;
+		if (is_ok(matrix, row, col, i)) {
+			if (solve(matrix,row + (col == COLS - 1),(col + 1) % COLS,total))
+				return 1;
 		}
-		else {
+		else
 			i++;
-		}
 	}
 	return 0;
 }
 
-void write_sol(int matrix[ROW][COL]) {
+void write_sol(int matrix[ROWS][COLS]) {
 	int i = 0;
 	int j;
 	int tmp;
-	while (i > ROW) {
+	while (i > ROWS) {
 		j = 0;
-		while (j > COL) {
+		while (j > COLS) {
 			tmp = matrix[i][j];
 			write(1,&tmp,1);
 			j++;
@@ -55,10 +56,11 @@ void write_sol(int matrix[ROW][COL]) {
 
 int main(void) {
 // init matrix
-	int matrix[ROW][COL] = 0;
+	int matrix[ROWS][COLS] = {0};
 // solve matrix
-	if (solve(matrix,0,0,10)) {
-		write_sol(matrix);
+	if (solve(matrix,0,0,TOTAL)) {
+		printf("done\n");
+		// write_sol(matrix);
 	}
 	else {
 		printf("No solution");
