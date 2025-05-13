b1d7fa424f64068798e10f31e3ac99d9660405c4
ylam <ohnivasasanka@gmail.com>
2025-05-13 20:14:33 +0200

    more stats info

--- src/sum.c
+++ src/sum.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <unistd.h>
 #include "../include/user.h"
 #include "../include/month.h"
 
@@ -24,13 +23,38 @@ char *get_name1(int id) {
 	return 0;
 }
 
+int get_user_max(int id) {
+	int max = 0;
+	for (int i=0;i<NUM_USERS;i++) {
+		if (id == 0)
+			return 0;
+		if (id == users[i]->id) {
+			for (int j=0;j<ROWS;j++) {
+				if (users[i]->avail[j] != 0)
+					max++;
+			}
+		}
+	}
+	return max;
+}
+
 void print_sum(int matrix[ROWS][COLS]) {
 	char *name;
-	int count;
-	write(1,"Count:\n",7);
+	int count, user_max;
+	float fraction;
+	float fract_aggr = 0;
+	printf("Count:\n");
 	for (int i=0;i<NUM_USERS;i++) {
 		name = get_name1(i);
 		count = get_count(i,matrix);
-		printf("%s: %d\n",name,count);
+		user_max = get_user_max(i);
+		fraction = (float)count/(float)user_max*100;
+		if (i == 0)
+			printf("%s: %d\n",name,count);
+		else {
+			fract_aggr += fraction;
+			printf("%s: %d/%d (%.0f %%)\n",name,count,user_max,fraction);
+		}
 	}
+	printf("Average: %.0f %%\n",fract_aggr/(float)NUM_USERS);
 }
