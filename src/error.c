103cb4114ae9ee8b8b51d0da36f8d9943ebb7d00
Ondrej Maly <omaly@c1r8s5.42prague.com>
2025-05-19 12:27:28 +0200

    new function for error handling in config

--- src/error.c
+++ src/error.c
@@ -1,13 +1,32 @@
 #include "../include/month.h"
-
+#include "../include/user.h"
+#include <stdio.h>
 int input_isnt_ok(int row, int col) {
 	if (row < 1 || col < 1)
 		return 1;
 	return 0;
 }
 
-int check_error(void) {
+int check_config(void) {
+	//check right input for scheduler/config/config.c
+	for (int i=0;i<NUM_USERS;i++) {
+		//check if int *avail has right input
+		for (int j=0;j<ROWS;j++) {
+			int day = users[i]->avail[j];
+			if ((day != 0)&&(day != 1)&&(day != 2)) {
+				printf("Error: user with id:%d has wrong input\n",users[i]->id);
+				return 1;
+			}
+
+		}
+	}
+	return 0;
+}
+
+int errors(void) {
 	if (input_isnt_ok(ROWS,COLS))
+		return 1;
+	if (check_config())
 		return 1;
 	return 0;
 }
--- src/main.c
+++ src/main.c
@@ -3,14 +3,14 @@
 #include <time.h>
 #include "../include/month.h"
 
-int check_error(void);
+int errors(void);
 void solve(int matrix[ROWS][COLS]);
 void print_matrix(int matrix[ROWS][COLS]);
 void print_stats(int matrix[ROWS][COLS]);
 
 int main(void) {
 	srand(time(NULL)); //seed randomness
-	if (check_error())
+	if (errors())
 		return 0;
 	int matrix[ROWS][COLS];
 	for (int i=0;i<ROWS;i++){
